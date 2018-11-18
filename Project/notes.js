ee22fbc7c1065b9de82ac091c760abd85c59c8d0
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2018-11-18 15:36:16 +0000

    automatically backed up by learn

---
+++ Project/notes.js
@@ -0,0 +1,27 @@
+// HTML
+
+/* <input>
+<buttonz> <nav> or <input type="radio">
+<div> for the picture on the right
+
+
+*/
+// CSS
+/*divs height and width must be higher than the form
+CSS grid one grid, one row for the header,
+2 columns grid maybe?left column is the form, right is the invite
+Flexbox??
+background-color is black or whatever
+background-image:
+class or id to switch between the options
+
+*/
+// JS
+/*
+1. form
+event Listener for typing
+
+2.The styling(formal, spring, art deco)
+onClick on button/div/whatever, the style changes for invitations
+
+*/
